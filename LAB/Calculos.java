ea0ceca7ea10fb12f5b485764afad0bd036edfe7
Adriano de Paula <adriano-de.paula@outlook.com>
2021-10-14 08:15:46 -0300

    Mudança adicionado arquivo Docker04Network

---
+++ LAB/Calculos.java
@@ -0,0 +1,13 @@
+/* som two values */
+ public class Calculos {
+    public static void main(String[] args) {
+
+        float Pparcela = -43;
+        float Sparcela = 54.6;
+
+        float Total = Pparcela  + Sparcela;
+
+        System.out.println("O resultado é " + Total);
+
+    }
+ }
